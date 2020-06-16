eeb54d6b01258351edc46f7daacbc1f83a959e1d
shazm12 <64892076+shazm12@users.noreply.github.com>
2020-06-16 14:28:23 +0530

    Updates

---
+++ precache-manifest.69295e717db9949dd50e1fff67abcdbc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "848d0ca9d72a942d61e6fd04597a89f4",
+    "url": "/weatherwebapi/index.html"
+  },
+  {
+    "revision": "c0d2cc073afa84f31fa1",
+    "url": "/weatherwebapi/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eaa5fc3b3b57903e8635",
+    "url": "/weatherwebapi/static/css/main.6dc85676.chunk.css"
+  },
+  {
+    "revision": "c0d2cc073afa84f31fa1",
+    "url": "/weatherwebapi/static/js/2.cb0cc6d6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weatherwebapi/static/js/2.cb0cc6d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa5fc3b3b57903e8635",
+    "url": "/weatherwebapi/static/js/main.b4b5eacf.chunk.js"
+  },
+  {
+    "revision": "b190fdaf9ecd62959d2c",
+    "url": "/weatherwebapi/static/js/runtime-main.40472589.js"
+  },
+  {
+    "revision": "8c295eeaf9495f0c160c5870b8c955e6",
+    "url": "/weatherwebapi/static/media/Lovelo-Black.8c295eea.woff"
+  },
+  {
+    "revision": "c5d58534600f319fc8660efc1644d371",
+    "url": "/weatherwebapi/static/media/background-cold.c5d58534.jpg"
+  },
+  {
+    "revision": "1ab301c5a48e136765f62bdd233f3caa",
+    "url": "/weatherwebapi/static/media/background-warm.1ab301c5.jpg"
+  }
+]);
